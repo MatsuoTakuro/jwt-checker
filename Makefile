f23e57e2db6933b72a7b61669ac0c48cafa8772c
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-12-11 13:38:52 +0900

    verify signaturby decrypting it with google's public key

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 run:
 	go run main.go
+
+google_pkey:
+	http https://accounts.google.com/.well-known/openid-configuration
+
+google_jwt_pkey:
+	http https://www.googleapis.com/oauth2/v3/certs
--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
+	"math/big"
+	"os"
 	"strings"
 
 	"github.com/MatsuoTakuro/jwt-checker/idtoken"
+	"github.com/MatsuoTakuro/jwt-checker/pubkey"
 )
 
 func main() {
 	rawData := strings.Split(idtoken.IDToken, ".")
-	rawh, rawp, _ := rawData[0], rawData[1], rawData[2]
-	fmt.Println("raw header: ", rawh)
-	fmt.Println("raw payload: ", rawp)
+	rawh, rawp, rawsig := rawData[0], rawData[1], rawData[2]
 
 	h, err := base64.RawURLEncoding.DecodeString(rawh)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	err = os.WriteFile("./idtoken/header.json", h, 0644)
 	if err != nil {
 		fmt.Println("error:", err)
 		return
@@ -25,7 +34,33 @@ func main() {
 		fmt.Println("error:", err)
 		return
 	}
+	err = os.WriteFile("./idtoken/payload.json", p, 0644)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
 
-	fmt.Println("header: ", string(h))
-	fmt.Println("payload: ", string(p))
+	rawpk := pubkey.NewOauth2Cert()
+	n, _ := base64.RawURLEncoding.DecodeString(rawpk.N)
+	e, _ := base64.RawURLEncoding.DecodeString(rawpk.E)
+	pk := &rsa.PublicKey{
+		N: new(big.Int).SetBytes(n),
+		E: int(new(big.Int).SetBytes(e).Int64()),
+	}
+
+	msg := sha256.Sum256([]byte(rawh + "." + rawp))
+
+	sig, err := base64.RawURLEncoding.DecodeString(rawsig)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	if err := rsa.VerifyPKCS1v15(pk, crypto.SHA256, msg[:], sig); err != nil {
+		fmt.Println("invalid id_token")
+	} else {
+		fmt.Println("valid id_token")
+		fmt.Println("header: ", string(h))
+		fmt.Println("payload: ", string(p))
+	}
 }
